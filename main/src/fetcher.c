110f7a20b51c883c2fb5efff00e6b1426efc6cd1
hrtowii <68852354+hrtowii@users.noreply.github.com>
2026-01-11 02:40:13 +0800

    tired

---
+++ main/src/fetcher.c
@@ -0,0 +1,8 @@
+// web fetcher
+// https://github.com/espressif/esp-idf/blob/v5.5.2/examples/protocols/esp_http_client/main/esp_http_client_example.c
+// api for bus: https://github.com/cheeaun/arrivelah
+// bus ids maybe hardcoded for now? just do like 972
+// 45009
+// https://arrivelah2.busrouter.sg/
+
+
--- main/src/main.c
+++ main/src/main.c
@@ -3,9 +3,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-
+#include "nvs_flash.h"
 #include "ssd1306.h"
 #include "fonts.h" 
+#include "wifi.h"
 // https://github.com/NUSGreyhats/greycat2k24-badge-public/blob/main/firmware/greycat_firmware/Src/hw/oled.c#L4
 static const char *TAG = "MAIN";
 
@@ -38,12 +39,27 @@ void oled_update_name(char *string) {
     
     ssd1306_UpdateScreen();
 }
-
+// https://github.com/espressif/esp-idf/blob/master/examples/system/freertos/basic_freertos_smp_usage/main/create_task_example.c
+void display_thread(void *arg) {
+    int task_id = (int)arg;
+    ESP_LOGI(TAG, "created task#%d", task_id);
+}
 
 void app_main(void) {
-
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    // nvs needed fr wifi
+    ESP_ERROR_CHECK(ret);
     oled_init();
+    if (CONFIG_LOG_MAXIMUM_LEVEL > CONFIG_LOG_DEFAULT_LEVEL) {
+        esp_log_level_set("wifi", CONFIG_LOG_MAXIMUM_LEVEL);
+    }
 
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
     char my_name[] = "goated meow"; 
     oled_update_name(my_name);
 
